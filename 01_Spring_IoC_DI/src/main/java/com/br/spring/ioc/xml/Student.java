ff1f0b4af3e1d5529396efbac19617cf159570ce
Junsu001 <junsu2752@gmail.com>
2024-10-14 17:05:11 +0900

    Initial commit

---
+++ 01_Spring_IoC_DI/src/main/java/com/br/spring/ioc/xml/Student.java
@@ -0,0 +1,15 @@
+package com.br.spring.ioc.xml;
+
+public class Student {
+
+	private String name;
+	private String academy;
+	private String classColor;
+	private Calculator calc;
+	
+	public Student() {}
+	
+	
+	
+	
+}
